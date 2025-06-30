8b30f0fb04d4db5a2508f33efb8ffefccbac7a4e
ihansaem <yellowstarr@dsm.hs.kr>
2025-06-30 19:58:23 +0900

    .

--- loginPage/src/main/java/com/example/loginpage/config/SecurityConfig.java
+++ loginPage/src/main/java/com/example/loginpage/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+      http.csrf();
     return http.build();
   }
 }
